c547049632528c750cadbeb0d6c70a3e6273dbf2
Guy Barrand <guy.barrand@gmail.com>
2018-05-26 09:53:07 +0200

    update

--- doc/h/introduction.h
+++ doc/h/introduction.h
@@ -2,30 +2,23 @@
 
 @page inlib_minuit_introduction Introduction
 
-  inlib_minuit is the collection of exlib examples related to Python put in a standalone package
- and distribution.
+  inlib_minuit is the collection of inlib examples related to the inlib/f2cmn minimisation class.
+ inlib/f2cmn is a modified version of the CERN-ROOT/TMinuit class in order to have a stand alone
+ C++ version of the Minuit package. (CERN-ROOT/TMinuit being itself a FORTRAN to C translation
+ of the FORTRAN CERNLIB/MINUIT package).
 
-  There are little applications written in C++, that activates Python through its library and
- a set of .py scripts that demonstrate how to activate inlib/exlib from a Python prompt. All
- these use the SWIG wrapping of some (but not all) classes of inlib and exlib.
+  In inlib/examples/cpp, there are:
+@verbatim
+   f2cmn.cpp      # example by using the "raw" minuit like interface.
+   fit.cpp        # a "gaussian+breit_wigner" fit by using a inlib::fit_func and
+                  # the inlib::f2cmn::fit_1D<> template function.
+   fit_poly.cpp   # a polynomial fit by using inlib::f2cmn::fit_1D().
+   fit_multi.cpp  # a multiple functions fit, by using the inlib::f2cmn::func_fitter::fit() API.
+@endverbatim
 
-  C++ apps (in 1.0.0) (source code in inlib_minuit/inexlib/exlib/examples/cpp) :
-@verbatim
-    hello_py.cpp            # to check Python activation from C++.
-    h1d_spy.cpp             # inlib histogram wrapped with swig.
-    plotter_spy_screen.cpp  # exlib plotting wrapped with swig.
-    mandel_py.cpp           # check calling a C++ function from Python.
-@endverbatim
- and .py scripts :
-@verbatim
-    h1d.py          # inlib histo.
-    rroot.py        # read an histo in a root file.
-    plotter.py      # plot an histo by using softinex graphics and plotting.
-    tree.py         # project a ntuple found in the pawdemo.root file and plot the histo.
-    waxml.py        # write histos at the AIDA XML file format.
-    polyhedron.py   # visualise a solid/shape boolean operation.
-    cfitsio_hst.py  # visualise an image in a fits file.
-@endverbatim
+  Someone sensitive to software engineering may appreciate the difference in number of lines of
+ code to embarque between inlib/f2cmn (around 6 kloc) and CERN-ROOT (around... too much) in order
+ to fit an histogram.
 
   See the README on github for instructions on how to build, install and run.
 
