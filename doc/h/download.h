a508a1b6039d141a1103d8c526d745dad37cf94e
Guy Barrand <guy.barrand@gmail.com>
2020-06-27 16:38:40 +0200

    update

--- doc/h/download.h
+++
@@ -1,9 +0,0 @@
-/**
-@page inlib_minuit_download Download
-
-  Source and some binaries are on github :
-<p>
-  <a href="https://github.com/gbarrand/inlib_minuit.git" target="_blank">https://github.com/gbarrand/inlib_minuit.git</a>
-</p>
-
-*/
